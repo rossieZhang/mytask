1ca91b5dd69aafd858a9756ab57fbf5fbe23e4d6
cola <896399229@qq.com>
2018-06-30 00:18:16 +0800

    cc

--- mytask/src/gotest/gotest_test.go
+++ mytask/src/gotest/gotest_test.go
@@ -8,40 +8,40 @@ import(
 //test one: normal input
 //常规输入测试一：存在只出现一次的字符 
 func TestTrain1(t *testing.T){
-	if i := Train("abccda"); i != 'b'{ 
-        	t.Error("测试一失败") 
-    	} else {
-		t.Log("测试一成功") 
+	if i := Train("abccda"); i != 'b'{
+		t.Error("测试一失败")
+	} else {
+		t.Log("测试一成功")
     }
 }
 
 //test two: normal input, each character appeared once 
 //常规输入测试二：不存在只出现一次的字符
 func TestTrain2(t *testing.T){
-	if i := Train("aabb"); i != '0'{ 
-        	t.Error("测试二失败") 
-    	} else {
-		t.Log("测试二成功") 
+	if i := Train("aabb"); i != '0'{
+		t.Error("测试二失败")
+	} else {
+		t.Log("测试二成功")
     }
 }
 
 //test three: normal input, only one character
 //常规输入测试三：字符串长度为1
 func TestTrain3(t *testing.T){
-	if i := Train("a"); i != 'a'{ 
-        	t.Error("测试三失败") 
-    	} else {
-		t.Log("测试三成功") 
+	if i := Train("a"); i != 'a'{
+		t.Error("测试三失败")
+	} else {
+		t.Log("测试三成功")
     }
 }
 
 //test four: abnormal input, ""
 //常规输入测试四：空字符
 func TestTrain4(t *testing.T){
-	if i := Train(""); i != '0'{ 
-        	t.Error("测试四失败") 
-    	} else {
-		t.Log("测试四成功") 
+	if i := Train(""); i != '0'{
+		t.Error("测试四失败")
+	} else {
+		t.Log("测试四成功")
     }
 }
 
